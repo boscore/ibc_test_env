e2c108e55715610dd4766ebe9c56104113afb21c
vonhenry <amorofroma@gmail.com>
2019-09-24 08:57:45 +0800

    update

--- multi_bps_per_chain/init_chains.sh
+++ multi_bps_per_chain/init_chains.sh
@@ -8,6 +8,10 @@ if [ "$bn" != "multi_bps_per_chain" ]; then
     return
 fi
 
+curl -X POST http://127.0.0.1:8002/v1/producer/schedule_protocol_feature_activations -d \
+     '{"protocol_features_to_activate": ["0ec7e080177b2c02b278d5088611686b49d739925a92d9bfcacd7fc6b74053bd"]}' | jq
+
+
 . ./../env.sh
 . ./../init.sh
 . ./../init_system_contracts.sh
